b8083fb289a779be40fd8f2ff05e733e06ebaebf
yousafsabir <yousafsabir430516@gmail.com>
2022-06-08 01:43:52 +0500

    test design

--- src/App.js
+++ src/App.js
@@ -2,10 +2,24 @@ import "./App.css";
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <h2 className="text-center">Hello</h2>
-            </header>
+        <div className="App text-slate-200 bg-slate-800 min-h-screen">
+            <nav className="max-w-5xl mx-auto my-0 py-2 px-1 flex justify-between items-center">
+                <h1 className="font-semibold text-2xl">Tailwind Setup</h1>
+                <ul className="flex gap-6">
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                    <li>
+                        <a href="/">About</a>
+                    </li>
+                    <li>
+                        <a href="/">Services</a>
+                    </li>
+                    <li>
+                        <a href="/">Contact Us</a>
+                    </li>
+                </ul>
+            </nav>
         </div>
     );
 }
